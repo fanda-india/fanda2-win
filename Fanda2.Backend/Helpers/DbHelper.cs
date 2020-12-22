028ea2ae2096bdc0695005215cca9ec9a376906d
Fanda <58879581+fanda-india@users.noreply.github.com>
2020-12-22 20:29:41 +0530

    Update DbHelper.cs

--- Fanda2.Backend/Helpers/DbHelper.cs
+++ Fanda2.Backend/Helpers/DbHelper.cs
@@ -10,7 +10,7 @@ namespace Fanda2.Backend.Helpers
         public static Expression<Func<T, bool>> AndAlso<T>(Expression<Func<T, bool>> expr1,
             Expression<Func<T, bool>> expr2)
         {
-            var body = Expression.AndAlso(expr1.Body, expr1.Body);
+            var body = Expression.AndAlso(expr1.Body, expr2.Body);
             var lambda = Expression.Lambda<Func<T, bool>>(body, expr1.Parameters[0]);
             return lambda;
         }
